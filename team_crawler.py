e07f258cad874ac98fc441b8fb9f3186e84fa2fa
EireneXu <xuanping.xu@gmail.com>
2017-02-26 13:46:57 +0800

    python2 migrate for team_crawler.py

--- team_crawler.py
+++ team_crawler.py
@@ -1,5 +1,7 @@
 import time
-from urllib.request import urlopen
+# from urllib.request import urlopen
+# python 2
+from urllib2 import urlopen
 import csv
 
 
@@ -7,9 +9,7 @@ with open("data/team_list.csv", "r+") as f:
     reader = csv.reader(f)
     teams = {row[0]: row[1] for row in reader if "team_href" not in row}
 
-
 for name, href in teams.items():
-
     print("Crawling data for team " + name + " at url:")
     print(href)
 
@@ -19,5 +19,3 @@ for name, href in teams.items():
         f.write(r)
 
     time.sleep(1)
-    # if index > 2:
-    #     break
