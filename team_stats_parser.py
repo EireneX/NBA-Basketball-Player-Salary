0ab207a8930336afeac0117f37c63976de849abf
EireneXu <xuanping.xu@gmail.com>
2017-02-26 13:53:07 +0800

    python2 migrate for team_stats_parser.py

--- team_stats_parser.py
+++ team_stats_parser.py
@@ -1,10 +1,17 @@
-import time
 from bs4 import BeautifulSoup, Comment
-from urllib.request import urlopen
-import json, os.path, csv, re
+# from urllib.request import urlopen
+# python 2
+from urllib2 import urlopen
+import os.path, csv, re
 
 def valid_filename(filename):
     return re.sub('[^\w\-_\. ]', '_', filename)
+
+
+def norm_txt(txt):
+    # removing non-ascii
+    return re.sub(r'[^\x00-\x7F]+',' ', txt)
+
 
 def parse_team_stats(team_name):
     for stats_id, table in tables_stats.items():
@@ -15,14 +22,17 @@ def parse_team_stats(team_name):
         if not os.path.isdir(folder_path):
             os.makedirs(folder_path)
 
-        with open(folder_path+valid_filename(team_name+".csv"), "w+", newline="") as f:
+        # python 3
+        # with open(folder_path+valid_filename(team_name+".csv"), "w+", newline="") as f:
+        # python 2
+        with open(folder_path + valid_filename(team_name + ".csv"), "wb") as f:
             writer = csv.writer(f, delimiter=",")
             # write header
-            writer.writerow([th.text for th in thead.find_all("tr")[-1].find_all("th")])
+            writer.writerow([norm_txt(th.text) for th in thead.find_all("tr")[-1].find_all("th")])
             # write stats
             tbody = table.find_all("tbody")[0]
             for tr in tbody.find_all("tr"):
-                writer.writerow([ele.text for ele in tr.find_all(["th", "td"])])
+                writer.writerow([norm_txt(ele.text) for ele in tr.find_all(["th", "td"])])
 
 
 # read all team information
@@ -37,20 +47,15 @@ for team_name, team_href in teams.items():
     tables_stats = {}
 
     r = open("data/teams/"+team_name+".html", "rb+").read()
-    soup = BeautifulSoup(r, "lxml")
-
+    # python 3
+    # soup = BeautifulSoup(r, "lxml")
+    # python 2
+    soup = BeautifulSoup(r, "html.parser")
     tables_all = []
 
     # Extract all tables in non-comments
     table = soup.find("div", id="content").find("table")
     tables_all.append(table)
-
-    # # Extract all tables in comments
-    # comments = soup.find_all(string=lambda text: isinstance(text, Comment))
-    # for c in comments:
-    #     soup_comment = BeautifulSoup(c, 'lxml')
-    #     table = soup_comment.find('table')
-    #     tables_all.append(table)
 
     for table in tables_all:
         tables_stats[table.get("id")] = table
